fc2760e943fcc1bdb5d7306cf4e837f3096dbc56
AsmaaHM <40245767+AsmaaHM@users.noreply.github.com>
2022-08-06 23:18:47 +0200

    migration files

---
+++ EmployeeManagement/Migrations/20220805180319_NotificationsTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class NotificationsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notification",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TranType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notification", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notification");
+        }
+    }
+}
